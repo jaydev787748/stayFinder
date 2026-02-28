53e07e17da2da4f1838978be58c604d65aba691e
= <=>
2026-03-01 04:11:28 +0530

    initlaizedvdatabase again

--- init/init.js
+++ init/init.js
@@ -7,7 +7,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wanderlust")
     .then(() => console.log("Connected to database"))
     .catch(err => console.log("Error in connecting to mongodb"));
 
-Listing.insertMany(data.data)
-    .then((res) => console.log("Initilazing mongodb Successfully"))
+Listing.insertMany(data)
+    .then((res) => console.log("Initialized mongodb Successfully"))
     .catch(err => console.log("Error in initlazing to databse ", err));
 
